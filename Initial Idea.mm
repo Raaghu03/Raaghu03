e3d84051c74791bae73bc0da4c4e7fbeac3a6420
Raaghu03 <93691147+Raaghu03@users.noreply.github.com>
2021-11-04 07:19:17 +0530

    Add files via upload
    Mindmap of Initial idea

---
+++ Initial Idea.mm
@@ -0,0 +1,132 @@
+<map version="freeplane 1.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Baby Vaccination Tracker?" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1635988117150">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Vaccination History" POSITION="right" ID="ID_454007740" CREATED="1635988126328" MODIFIED="1635988140316">
+<node TEXT="Image of Vaccine" ID="ID_149055638" CREATED="1635988674457" MODIFIED="1635988696550"/>
+<node TEXT="Date, Name,..." ID="ID_41947822" CREATED="1635988699628" MODIFIED="1635988732521"/>
+<node TEXT="Pediatrician" ID="ID_448695628" CREATED="1635988812513" MODIFIED="1635988817759"/>
+</node>
+<node TEXT="Vaccination Schedule" POSITION="left" ID="ID_201175402" CREATED="1635988144984" MODIFIED="1635988156576">
+<node TEXT="Reminders" ID="ID_901285865" CREATED="1635988429516" MODIFIED="1635988442021">
+<node TEXT="E-Mail" ID="ID_290538282" CREATED="1635988444940" MODIFIED="1635988448717"/>
+<node TEXT="Messages?" ID="ID_868400898" CREATED="1635988451751" MODIFIED="1635988454442"/>
+</node>
+</node>
+<node TEXT="Growth Tracker" POSITION="right" ID="ID_332140248" CREATED="1635988230420" MODIFIED="1635988235428">
+<node TEXT="Height and Weight" ID="ID_1557131109" CREATED="1635988928770" MODIFIED="1635988933527"/>
+</node>
+<node TEXT="User Management" POSITION="left" ID="ID_290049309" CREATED="1635988253406" MODIFIED="1635988260360">
+<node TEXT="Admin" ID="ID_713626063" CREATED="1635988261301" MODIFIED="1635988263534"/>
+<node TEXT="End Users" ID="ID_1965134371" CREATED="1635988265852" MODIFIED="1635988274519">
+<node TEXT="Profile Management" ID="ID_1720501173" CREATED="1635988499456" MODIFIED="1635988504573"/>
+<node TEXT="Vaccination details update" ID="ID_815758635" CREATED="1635988505621" MODIFIED="1635988513215"/>
+<node TEXT="Reports and Statistics" ID="ID_1722969430" CREATED="1635988514496" MODIFIED="1635988548509"/>
+<node TEXT="Pediatrician management" ID="ID_1875386440" CREATED="1635988742782" MODIFIED="1635988759736">
+<node TEXT="Insert" ID="ID_462176328" CREATED="1635988761246" MODIFIED="1635988763765"/>
+<node TEXT="Edit" ID="ID_896074179" CREATED="1635988772030" MODIFIED="1635988773227"/>
+</node>
+<node TEXT="Baby Management" ID="ID_1107841275" CREATED="1635988838750" MODIFIED="1635988842625">
+<node TEXT="Add, Edit" ID="ID_920892922" CREATED="1635988851893" MODIFIED="1635988866865"/>
+<node TEXT="Baby Photo" ID="ID_1400041768" CREATED="1635989059361" MODIFIED="1635989088659"/>
+</node>
+</node>
+</node>
+<node TEXT="Vaccination Info Database" POSITION="right" ID="ID_649399761" CREATED="1635988289438" MODIFIED="1635989124323">
+<icon BUILTIN="full-2"/>
+<node TEXT="Stage 2 - General Information about the Vaccinations" ID="ID_880261106" CREATED="1635988937839" MODIFIED="1635988949011"/>
+<node TEXT="Based on countries?" ID="ID_1558953595" CREATED="1635988949914" MODIFIED="1635988954688"/>
+<node TEXT="Information about mandatory vaccinations and optional?" ID="ID_327991404" CREATED="1635988957777" MODIFIED="1635988975667"/>
+<node TEXT="Information about available alternatives?" ID="ID_1643786685" CREATED="1635988980628" MODIFIED="1635988989408"/>
+</node>
+<node TEXT="Technology" POSITION="left" ID="ID_1939837451" CREATED="1635988314685" MODIFIED="1635988318290">
+<node TEXT="Front End: Angular" ID="ID_1630091580" CREATED="1635988327126" MODIFIED="1635988334696"/>
+<node TEXT="Database: SQL" ID="ID_924854996" CREATED="1635988335465" MODIFIED="1635988340806">
+<node TEXT="Entity Framework" ID="ID_951838304" CREATED="1635988407462" MODIFIED="1635988415041"/>
+</node>
+<node TEXT="Backend: .Net Core 5" ID="ID_584962133" CREATED="1635988342450" MODIFIED="1635988360077"/>
+<node TEXT="Documentation and Help?" ID="ID_705284925" CREATED="1635988368056" MODIFIED="1635988374479"/>
+<node TEXT="Source Control: GIT" ID="ID_622984209" CREATED="1635988380971" MODIFIED="1635988390363"/>
+</node>
+<node TEXT="Testing" POSITION="left" ID="ID_1066827675" CREATED="1635988319302" MODIFIED="1635989144144">
+<icon BUILTIN="full-9"/>
+</node>
+</node>
+</map>
